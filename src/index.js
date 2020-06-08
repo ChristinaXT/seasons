9c56e5f264ab2e10f7666f9d3701ee044d42f3fc
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-08 17:21:23 -0400

    hh

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+class App extends React.Component 
+//React says we have to define render!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
