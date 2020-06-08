30e4c57e70a2b956cdd37a5df9e83ecf759e01f6
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-08 17:02:28 -0400

    latitude

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi There</div>;
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+
+  return <div>Latitude: </div>;
 };
 
 
 ReactDOM.render(
-  <App />, document.querySelector('#root')
-
-);
+  <App />, document.querySelector('#root'));
