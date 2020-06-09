5900bf0447648172eb922d1277f820593dd60287
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-09 09:45:21 -0400

    added render

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ class App extends React.Component {
     super(props);
 
     this.state = { lat: null };
-  }
 
-//React says we have to define render!!
-  render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
       (err) => console.log(err)
     );
-    return <div>Latitude: </div>;
+  }
+
+//React says we have to define render!!
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
