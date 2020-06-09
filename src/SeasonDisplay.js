0186724c8d4839caa31efe73672d92683d675ea7
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-09 15:36:10 -0400

    added update to display

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,8 +10,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
 
-   return <div>Season Display</div>
-};
+   return (
+     <div>
+      {text}
+     </div>
+    );
+  };
 
 export default SeasonDisplay;
