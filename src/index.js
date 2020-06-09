586b59ad868d7b365c58490e86d5faf8bd2aa23d
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-09 12:42:57 -0400

    rerender

--- src/index.js
+++ src/index.js
@@ -19,6 +19,14 @@ class App extends React.Component {
     );
   }
 
+componentDidMount() {
+  console.log('My component was rendered to the screen');
+}
+
+componentDidUpdate() {
+  console.log('My component was just updated - it rerendered!');
+}
+
 //React says we have to define render!!
   render() {
     if (this.state.errorMessage && !this.state.lat) {
