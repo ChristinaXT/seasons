195b85b107114ff75e33d6ba94ebf73388c18c78
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-09 15:13:07 -0400

    display

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+
    return <div>Season Display</div>
 };
 
