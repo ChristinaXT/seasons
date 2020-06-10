f7aa2248c3645e48ae99eac8d4830f081aba10ae
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-10 14:19:46 -0400

    spin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   state = { lat: null, errorMessage: '' };
