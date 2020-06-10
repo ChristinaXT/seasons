61a6f6bf81383f78d5a0b9c63cc014145c382a7a
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-10 14:17:34 -0400

    export

--- src/spinner.js
+++ src/spinner.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Spinner = () => {
   return (
     <div className="ui active dimmer">
-     <div className="ui big text loader"
-       Loading...
-     </div>
+      <div className="ui big text loader"Loading...</div>
     </div>
   );
 };
+
+export default Spinner;
